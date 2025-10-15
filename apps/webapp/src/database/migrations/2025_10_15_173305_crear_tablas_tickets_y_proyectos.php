fe8adf34c835f374b53e05e4b018db7cea7b314a
Octavio <octabau02@outlook.com>
2025-10-15 14:30:32 -0600

    fix: Cambios en tablas

--- apps/webapp/src/database/migrations/2025_10_15_173305_crear_tablas_tickets_y_proyectos.php
+++ apps/webapp/src/database/migrations/2025_10_15_173305_crear_tablas_tickets_y_proyectos.php
@@ -6,40 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-
     public function up(): void
-    {
-        Schema::create('proyectos', function (Blueprint $table) {
-            $table->unsignedBigInteger('proyecto_id')->autoIncrement()->primary();
-            $table->foreignId('cliente_id')->constrained('clientes', 'cliente_id');
-            $table->text('nombre');
-            $table->text('descripcion');
-            $table->enum('status', ['activo', 'inactivo', 'eliminado'])->default('activo')->comment('activo, inactivo, eliminado');
-            $table->text('motivo_eliminacion');
-            $table->unsignedBigInteger('registro_autor_id');
-            $table->timestamp('registro_fecha');
-            $table->unsignedBigInteger('actualizacion_autor_id')->nullable();
-            $table->timestamp('actualizacion_fecha')->nullable();
-        });
-    }
-
-    public function down(): void
-    {
+    {        
         Schema::create('tickets', function (Blueprint $table){
             $table->unsignedBigInteger('ticket_id')->autoIncrement()->primary();
-            $table->foreignId('cliente_id')->constrained('clientes', 'cliente_id');
-            $table->foreignId('proyecto_id')->constrained('proyectos', 'proyecto_id');
-            $table->foreignId('etiqueta_id')->constrained('etiquetas', 'etiqueta_id');
-            $table->foreignId('usuario_asignado_id')->constrained('sys_usuarios', 'usuario_id');
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreignId('proyecto_id');
+            $table->foreignId('etiqueta_id');
+            $table->foreignId('usuario_asignado_id');
             $table->unsignedBigInteger('folio');
             $table->text('titulo');
             $table->text('descripcion');
-            $table->enum('prioridad', ['baja','media','alta','urgente'])->comment('baja','media','alta','urgente');
+            $table->enum('prioridad', ['Baja','Media','Alta','Urgente'])->comment('Baja','Media','Alta','Urgente');
             $table->text('status');
             $table->unsignedBigInteger('registro_autor_id');
             $table->timestamp('registro_fecha');
             $table->unsignedBigInteger('actualizacion_autor_id')->nullable();
             $table->timestamp('actualizacion_fecha')->nullable();
+
+            $table->foreign('cliente_id')->references('cliente_id')->on('clientes');
+            $table->foreign('proyecto_id')->references('proyecto_id')->on('proyectos');
+            $table->foreign('etiqueta_id')->references('etiqueta_id')->on('etiquetas');
+            $table->foreign('usuario_asignado_id')->references('usuario_id')->on('sys_usuarios');
         });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
     }
 };
