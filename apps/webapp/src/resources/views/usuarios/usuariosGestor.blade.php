46eff326d96ec93381163766d3bdf171d13551e6
Octavio <octabau02@outlook.com>
2025-10-30 14:57:56 -0600

    feat: Se agrego el agregar perfiles a usuarios

--- apps/webapp/src/resources/views/usuarios/usuariosGestor.blade.php
+++ apps/webapp/src/resources/views/usuarios/usuariosGestor.blade.php
@@ -4,7 +4,7 @@
 
 @section('contenido')
   <div id="app">
-    <div class="">
+    <div class="modulo-encabezado">
       <div class="cont-buscador">
         <input type="text" name="usuario" id="usuario" class="input" v-model="busqueda" @change="buscar()" placeholder="Buscar usuarios..."></input>
       </div>
@@ -25,7 +25,9 @@
         <tr v-for="usuario in usuarios" :key="usuario.usuarioId">
           <td>@{{ usuario.usuario }}</td>
           <td>@{{ usuario.email }}</td>
-          <td>@{{ usuario.nombrePerfil }}</td>
+          <td>
+            <span v-if="!usuario.nombrePerfiles.length">-</span>
+            @{{ usuario.nombrePerfiles[0] }} <span v-if="usuario.nombrePerfiles.length > 1">+@{{ usuario.nombrePerfiles.length - 1 }}</span></td>
           <td>
             <span class="badge" :class="badgeStatus(usuario.status)">@{{ usuario.status }}</span>
           </td>
@@ -68,10 +70,14 @@
           <span class="error" v-if="erroresModal.password">@{{ erroresModal.password[0] }}</span>
         </div>
         <div class="campo">
-          <label class="etiqueta" for="perfil">Perfil</label>
-          <select class="input" name="perfil" id="perfil" v-model="formUsuario.perfil"></select>
-            
-          <span class="error" v-if="erroresModal.perfil">@{{ erroresModal.perfil[0] }}</span>
+          <label class="etiqueta" for="perfiles">Perfil</label>
+          <div class="contenedor-checklist">
+            <div class="item-contenedor" v-for="perfil in perfiles" :key="perfil.perfil_id">
+              <input type="checkbox" :id="'perfil-' + perfil.perfil_id" v-model="formUsuario.perfiles" :value="perfil.perfil_id">
+              <label :for="'perfil-' + perfil.perfil_id">@{{ perfil.nombre }}</label>
+            </div>
+          </div>
+          <span class="error" v-if="erroresModal.perfiles">@{{ erroresModal.perfil[0] }}</span>
         </div>
       </form>
     </modal-componente>
@@ -114,12 +120,13 @@
             nombre: '',
             email: '',
             password: '',
-            perfil: '',
+            perfiles: [],
           },
           formEliminar: {
             motivo: ''
           },
-          token: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+          token: '{{ csrf_token() }}',
+          perfiles: {{ Js::from($perfiles) }},
           usuarios: null,
           usuario: null,
           erroresModal: {},
@@ -137,6 +144,7 @@
           this.formUsuario.nombre = '';
           this.formUsuario.email = '';
           this.formUsuario.password = '';
+          this.formUsuario.perfiles = [];
           this.mostrarModal = true;
         },
   
@@ -147,6 +155,7 @@
           this.formUsuario.nombre = this.usuario.usuario;
           this.formUsuario.email = this.usuario.email;
           this.formUsuario.password = ''; 
+          this.formUsuario.perfiles = this.usuario.idPerfiles || [];
           this.mostrarModal = true;
         },
   
