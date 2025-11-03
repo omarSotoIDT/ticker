5810e43120705d9a57f6d457159526b4578ae55a
Octavio <octabau02@outlook.com>
2025-11-03 13:45:45 -0600

    refactor: modificacion en modal

--- apps/webapp/src/public/componentes/modal.js
+++ apps/webapp/src/public/componentes/modal.js
@@ -2,14 +2,14 @@ var modal = {
   template: `
     <transition name="animation-fade">
       <div v-if="mostrar" class="fondo-modal" @click.self="close">
-        <div class="modal">
+        <div class="modal" :class="claseModal">
           <button class="cerrar-modal" @click="close">&times;</button>
           <h2 class="titulo-modal">{{ titulo }}</h2>
           <p class="subtitulo-modal">{{ subtitulo }}</p>
           <slot></slot>
           <div class="acciones-modal" v-if="mostrarBotones">
               <button @click="close" class="btn secondary-btn">Cancelar</button>
-              <button @click="$emit('confirmar')" class="btn action-btn">{{ textoConfirmacion }}</button>
+              <button @click="$emit('confirmar')" class="btn primary-btn">{{ textoConfirmacion }}</button>
           </div>
         </div>
       </div>
@@ -21,6 +21,7 @@ var modal = {
     subtitulo: { type: String, default: "" },
     textoConfirmacion: { type: String, default: "Aceptar" },
     mostrarBotones: { type: Boolean, default: true },
+    claseModal: { type: String },
   },
   emits: ["update:mostrar", "limpiar", "confirmar"],
   methods: {
