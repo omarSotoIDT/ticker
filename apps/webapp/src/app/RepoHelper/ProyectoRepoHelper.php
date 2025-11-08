2c8220008d4c31dc11d66d7686516fcecbff747b
enriqueCTA <e.cta.bv@gmail.com>
2025-11-08 12:08:02 -0600

    fix: correcion de filtro de status

--- apps/webapp/src/app/RepoHelper/ProyectoRepoHelper.php
+++ apps/webapp/src/app/RepoHelper/ProyectoRepoHelper.php
@@ -9,7 +9,7 @@ class ProyectoRepoHelper
     public static function aplicarFiltros(Builder $query, array $filters): Builder
     {
         if (!empty($filters['status'])) {
-            $query->where('p.status', $filters['status']);
+            $query->whereIn('p.status', $filters['status']);
         }
 
         if (!empty($filters['cliente_id'])) {
