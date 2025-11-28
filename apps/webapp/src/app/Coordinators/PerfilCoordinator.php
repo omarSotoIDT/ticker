dcd93d79ba7ff66acebec6f34c84a4a7e7a9fe81
JesusH66 <jesushernandez6656@gmail.com>
2025-11-27 18:03:41 -0600

    refactor: refactoriza el código del coordinator del módulo Perfiles

--- apps/webapp/src/app/Coordinators/PerfilCoordinator.php
+++ apps/webapp/src/app/Coordinators/PerfilCoordinator.php
@@ -13,58 +13,28 @@ class PerfilCoordinator
     {
         $perfiles = PerfilService::obtenerPerfiles($filtros, 10, $paginate);
 
-        if ($paginate && $perfiles instanceof \Illuminate\Pagination\LengthAwarePaginator) {
-            $perfilIds = $perfiles->pluck('perfil_id')->toArray();
-            $permisosPorPerfil = PermisoService::obtenerPermisosPerfiles($perfilIds);
+        $coleccion = $paginate
+            ? collect($perfiles->items())
+            : collect($perfiles);
 
-            $perfiles->getCollection()->transform(function ($perfil) use ($permisosPorPerfil) {
-                $perfil->permisos = $permisosPorPerfil
-                    ->get($perfil->perfil_id, collect())
-                    ->pluck('permiso_id')
-                    ->toArray();
-                return $perfil;
-            });
+        $perfilIds = $coleccion->pluck('perfil_id')->toArray();
 
-            return [
-                'perfiles' => $perfiles->items(),
-                'perfiles_sin_paginar' => null,
-                'links' => $perfiles->linkCollection(),
-                'permisos' => PermisoService::obtenerPermisos(),
-            ];
-        } else {
-            if (!empty($perfiles) && is_object($perfiles[0])) {
-                $perfilIds = array_map(function ($p) {
-                    return $p->perfil_id;
-                }, $perfiles);
-            } else {
-                $perfilIds = array_column($perfiles, 'perfil_id');
-            }
+        $permisosPorPerfil = PermisoService::obtenerPermisosPerfiles($perfilIds);
 
-            $permisosPorPerfil = PermisoService::obtenerPermisosPerfiles($perfilIds);
+        $coleccion->transform(function ($perfil) use ($permisosPorPerfil) {
+            $perfil->permisos = $permisosPorPerfil
+                ->get($perfil->perfil_id, collect())
+                ->pluck('permiso_id')
+                ->toArray();
+            return $perfil;
+        });
 
-            $perfilesConPermisos = array_map(function ($perfil) use ($permisosPorPerfil) {
-                if (is_object($perfil)) {
-                    $perfil->permisos = $permisosPorPerfil
-                        ->get($perfil->perfil_id, collect())
-                        ->pluck('permiso_id')
-                        ->toArray();
-                    return $perfil;
-                } else {
-                    $perfil['permisos'] = $permisosPorPerfil
-                        ->get($perfil['perfil_id'], collect())
-                        ->pluck('permiso_id')
-                        ->toArray();
-                    return $perfil;
-                }
-            }, $perfiles);
-
-            return [
-                'perfiles' => null,
-                'perfiles_sin_paginar' => $perfilesConPermisos,
-                'links' => null,
-                'permisos' => PermisoService::obtenerPermisos(),
-            ];
-        }
+        return [
+            'perfiles' => $paginate ? $coleccion : null,
+            'perfiles_sin_paginar' => $paginate ? null : $coleccion,
+            'links' => $paginate ? $perfiles->linkCollection() : null,
+            'permisos' => PermisoService::obtenerPermisos(),
+        ];
     }
 
     public static function crearPerfil(array $datos)
